775a25b19340118fb6022023657597f4d5c63f54
Sylvia <sylviad1221@gmail.com>
2016-09-27 14:43:35 -0400

    refactor code for completed task

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,4 @@
 // Check off specific todos by clicking
 $("li").click(function(){
-  // if li is gray
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-    // turn it black
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    })
-  }
-// else
-  else {
-    // turn it gray
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    })
-  }
+  $(this).toggleClass("completed");
 });
